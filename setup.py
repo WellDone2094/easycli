d0df25dbd671e5f49822b42e94603280bcf24185
Andrea Benfatti <WellDone2094@gmail.com>
2018-06-29 18:23:06 +0100

    Update: setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ from setuptools import setup, find_packages
 setup(
     name="climb",
     version='0.1',
+    author="Andrea Benfatti",
+    author_email="welldone2094@gmail.com",
+    description="Climb is a library that allow you to build CLI tools.",
+    url="https://github.com/WellDone2094/climb",
     license='GPLv3',
     install_requires=[],
     packages=find_packages()
